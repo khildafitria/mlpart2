e9f4a1e2fd2f4b7a7d8530b56b423e27746b21fb
khildafitria <149028314+khildafitria@users.noreply.github.com>
2023-12-31 08:35:53 +0700

    Update uasml.py

--- uasml.py
+++ uasml.py
@@ -61,7 +61,7 @@ def parse_list(x):
     elif len(x) > 1:
         return ", ".join(x)
 
-def return_item_df(item_antecedents):
+def return_item_bakery(item_antecedents):
     data = rules[["antecedents", "consequents"]].copy()
      
     data["antecedents"] = data["antecedents"].apply(parse_list)
