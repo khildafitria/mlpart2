ee0e262161d8d36b92e382f483d7c9121598987b
khildafitria <149028314+khildafitria@users.noreply.github.com>
2023-12-31 13:55:33 +0700

    Update uasml.py

--- uasml.py
+++ uasml.py
@@ -13,13 +13,13 @@ bakery["day"] = bakery['Date'].dt.weekday
 bakery["month"].replace([i for i in range(1, 12 + 1)], ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustur","September","Oktober","November","Desember"], inplace=True)
 bakery["day"].replace([i for i in range(6 + 1)], ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"],inplace=True)
 
-st.title("UAS Grocery Basket Analysis Algoritma Apriori")
+st.title("UAS Transaction from a bakery Algoritma Apriori")
 
-def get_data( month ='' , day = ''):
-    data = bakery.copy()
-    filtered = data.loc[
-        (data["month"].str.contains(month.title())) &
-        (data["day"].str.contains(day.title()))
+def get_bakery( month ='' , day = ''):
+    bakery = bakery.copy()
+    filtered = bakery.loc[
+        (bakery["month"].str.contains(month.title())) &
+        (bakery["day"].str.contains(day.title()))
     ]
     return filtered if filtered.shape[0] else "No Result!"
 
@@ -33,7 +33,7 @@ def user_input_features():
 
 item, month, day = user_input_features()
 
-data = get_data(month, day)
+data = get_bakery(month, day)
 
 def encode(x):
     if x <= 0:
@@ -42,7 +42,7 @@ def encode(x):
         return 1
     
 if type(bakery) != type ("No Result"):
-    item_count = data.groupby(['Member_number', 'itemDescription'])["itemDescription"].count().reset_index(name="Count")
+    item_count = bakery.groupby(['Member_number', 'itemDescription'])["itemDescription"].count().reset_index(name="Count")
     item_count_pivot = item_count.pivot_table(index='Member_number', columns='itemDescription', values='Count', aggfunc='sum').fillna(0) 
     item_count_pivot = item_count_pivot.applymap(encode)
 
@@ -62,14 +62,14 @@ def parse_list(x):
     elif len(x) > 1:
         return ", ".join(x)
 
-def return_item_df(item_antecedents):
+def return_item_bakery(item_antecedents):
     bakery = rules[["antecedents", "consequents"]].copy()
      
     bakery["antecedents"] = bakery["antecedents"].apply(parse_list)
     bakery["consequents"] = bakery["consequents"].apply(parse_list)
 
-    return list(data.loc[bakery["antecedents"] == item_antecedents].iloc[0,:])
+    return list(bakery.loc[bakery["antecedents"] == item_antecedents].iloc[0,:])
 
 if type(bakery) != type("No Result!"):
     st.markdown("Hasil Rekomendasi : ")
-    st.success(f"Jika Konsumen Membeli **{item}**, maka membeli **{return_item_df(item)[1]}** secara bersamaan")
+    st.success(f"Jika Konsumen Membeli **{item}**, maka membeli **{return_item_bakery(item)[1]}** secara bersamaan")
