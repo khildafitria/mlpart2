7399a100944b0cd23dff0655a46bd909fd6c60d7
khildafitria <149028314+khildafitria@users.noreply.github.com>
2023-12-31 08:59:25 +0700

    Update uasml.py

--- uasml.py
+++ uasml.py
@@ -15,7 +15,7 @@ bakery["day"].replace([i for i in range(6 + 1)], ["senin","Selasa","Rabu","Kamis
 
 st.title("UAS Transaction from a bakery Algoritma Apriori")
 
-def get_data( month ='' , day = ''):
+def get_bakery( month ='' , day = ''):
     bakery = bakery.copy()
     filtered = bakery.loc[
         (bakery["month"].str.contains(month.title())) &
